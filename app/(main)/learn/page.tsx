6984e715cde85a849e495e82beedd3ae3301fd75
Guilherme Alves <iamalves.jpg@gmail.com>
2024-04-16 01:40:28 -0300

    learn-page(feat):create header

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,6 +1,24 @@
+import { FeedWrapper } from "@/components/feed-wrapper"
+import { StickyWrapper } from "@/components/sticky-wrapper"
+import { Header } from "./header"
+import { UserProgress } from "@/components/user-progress"
+import { title } from "process"
+
 const Learn = () => {
   return (
-    <div>Learn</div>
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: "Japanese", imageSrc: "/jp.svg"}}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="Japanese"/>
+      </FeedWrapper>
+    </div>
   )
 }
 
