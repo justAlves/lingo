ee6c3a403a41e8855a83a659a0f30150a0bbf9a3
Guilherme Alves <iamalves.jpg@gmail.com>
2024-04-15 21:26:36 -0300

    button(feat):create buttons variants

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,19 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <Button size={"lg"} variant={"destructive"}>Click Me</Button>
+    <div className="flex flex-col gap-4 w-56 p-4 flex-wrap">
+      <Button size={"lg"} variant={"default"}>Click Me</Button>
+      <Button size={"lg"} variant={"primary"}>Click Me</Button>
+      <Button size={"lg"} variant={"primaryOutline"}>Click Me</Button>
+      <Button size={"lg"} variant={"secondary"}>Click Me</Button>
+      <Button size={"lg"} variant={"secondaryOutline"}>Click Me</Button>
+      <Button size={"lg"} variant={"danger"}>Click Me</Button>
+      <Button size={"lg"} variant={"dangerOutline"}>Click Me</Button>
+      <Button size={"lg"} variant={"super"}>Click Me</Button>
+      <Button size={"lg"} variant={"superOutline"}>Click Me</Button>
+      <Button size={"lg"} variant={"ghost"}>Click Me</Button>
+      <Button size={"lg"} variant={"sidebar"}>Click Me</Button>
+      <Button size={"lg"} variant={"sidebarOutline"}>Click Me</Button>
     </div>
   );
 }
