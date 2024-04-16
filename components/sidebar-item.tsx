7a643274d224ef22dc61df566997c3b7b1b1a6f7
Guilherme Alves <iamalves.jpg@gmail.com>
2024-04-16 13:29:37 -0300

    user-progress(feat): create change course function

--- components/sidebar-item.tsx
+++ components/sidebar-item.tsx
@@ -3,6 +3,7 @@
 import { usePathname } from "next/navigation";
 import { Button } from "./ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 type Props = {
     label: string;
@@ -15,15 +16,17 @@ export const SidebarItem = ({label, iconSrc, href}: Props) => {
     const active = pathname === href;
 
   return (
-    <Button variant={active ? "sidebarOutline" : "sidebar"} className="justify-start h-[52px]">
-        <Image
-            src={iconSrc}
-            alt={label}
-            className="mr-5"
-            height={32}
-            width={32}
-        />
-        {label}
-    </Button>
+    <Link href={href}>
+      <Button variant={active ? "sidebarOutline" : "sidebar"} className="w-full justify-start h-[52px]">
+          <Image
+              src={iconSrc}
+              alt={label}
+              className="mr-5"
+              height={32}
+              width={32}
+          />
+          {label}
+      </Button>
+    </Link>
   )
 }
--- database/queries.ts
+++ database/queries.ts
@@ -2,7 +2,7 @@ import { cache } from "react";
 import db from "./drizzle";
 import { auth } from "@clerk/nextjs";
 import { eq } from "drizzle-orm";
-import { userProgress } from "./schema";
+import { courses, userProgress } from "./schema";
 
 export const getUserProgress = cache(async () => {
     const {userId} = await auth();
@@ -25,4 +25,12 @@ export const getCourses = cache(async () => {
     const data = await db.query.courses.findMany()
     
     return data;
+})
+
+export const geteCourseById = cache(async (courseId: number) => {
+    const data = await db.query.courses.findFirst({
+        where: eq(courses.id, courseId)
+    })
+
+    return data
 })
